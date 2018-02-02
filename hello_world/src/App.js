927b4cc7af6131c51997e9876a18625af269387f
Lena <fatmeow@gmail.com>
2018-02-02 03:21:21 -0500

    First react page with hello component

---
+++ hello_world/src/App.js
@@ -0,0 +1,19 @@
+// bring in React and Component from React
+import React, {Component} from 'react';
+
+// define our Hello Component
+class Hello extends Component {
+  // what should the component render?
+  render() {
+
+    // make sure to return some UI
+    return (
+      <div>
+        <h1>Hello World! </h1>
+        <h3>It is time for tea</h3>
+      </div>
+    );
+  }
+}
+
+export default Hello
