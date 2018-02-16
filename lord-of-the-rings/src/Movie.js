4bfab396d4a9f821768a8a02e9882e668eb17af4
Lena <fatmeow@gmail.com>
2018-02-16 06:20:09 -0500

    Update lord of rings app

--- lord-of-the-rings/src/Movie.js
+++ lord-of-the-rings/src/Movie.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Movie extends Component {
   render() {
     return (
-      //add JSX here
+      <div>
+        <h1>The Lors of the Rings: {this.props.title}</h1>
+        <p>{this.props.hours}h {this.props.minutes}min</p>
+      </div>
     )
   }
 }
