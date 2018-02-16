caf7a757ad638979cd396870aaf75f8909469262
Lena <fatmeow@gmail.com>
2018-02-16 06:19:37 -0500

    Use state to add value render ui

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -3,6 +3,26 @@ import React, {Component} from 'react';
 
 // define our Hello Component
 class Hello extends Component {
+  //constructor - what should happen when the component is first created
+  constructor (props) {
+    // make call to parent class' (Component) constructor
+    super()
+    // define an initial state
+    this.state = { moodPoints: 1 }
+  }
+  increaseMood(e) {
+      if (this.state.moodPoints < 10){
+        this.setState({
+          moodPoints: this.state.moodPoints + 1
+        })
+      } else {
+        this.setState({
+          moodPoints: 1
+        })
+      }
+
+  }
+
   // what should the component render?
   render() {
 
@@ -12,6 +32,8 @@ class Hello extends Component {
         <h1>Hello {this.props.name}! </h1>
         <p>You are {this.props.age} years old.</p>
         <p>You love: {this.props.animals[0]}</p>
+        <p>You are this happy: {this.state.moodPoints}</p>
+        <button onClick={(e) => this.increaseMood(e)}>Cheer up!</button>
       </div>
     );
   }
