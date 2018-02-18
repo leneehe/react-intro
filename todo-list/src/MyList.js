5e6495492be529f35c2e8034e1e366c84b45d6ca
Lena <fatmeow@gmail.com>
2018-02-18 00:15:42 -0500

    Add props to ListItem inside MyList

--- todo-list/src/MyList.js
+++ todo-list/src/MyList.js
@@ -8,7 +8,7 @@ class MyList extends Component {
       <div>
       <h1> Things I should stop procrastinating: </h1>
       <ul>
-        <ListItem/>
+        <ListItem doThis="Buy ice cream" />
       </ul>
       </div>
     )
