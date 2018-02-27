abc49e629be19a31c0a87ad1b408d8ef201b7b20
Lena <fatmeow@gmail.com>
2018-02-27 16:55:35 -0500

    Rewrite Todo list's ListItem to a functional component instead of class

--- todo-list/src/ListItem.js
+++ todo-list/src/ListItem.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class ListItem extends Component {
-  render() {
-    return (
-      <li>{this.props.doThis}</li>
-    )
-  }
-}
+// class ListItem extends Component {
+//   render() {
+//     return (
+//       <li>{this.props.doThis}</li>
+//     )
+//   }
+// }
+
+//rewrite class component to a functional component
+const ListItem = props => (
+  <li>{props.doThis}</li>
+)
 
 export default ListItem;
