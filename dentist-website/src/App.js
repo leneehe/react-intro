8deb40a3cc704487a1a10147cce4a49d20774d3a
Lena <fatmeow@gmail.com>
2018-03-10 16:29:16 -0500

    Create dentist website basic page

---
+++ dentist-website/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Dentist Website</h1>
+        </header>
+        <p className="App-intro">
+          Welcome to the dentist office
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
