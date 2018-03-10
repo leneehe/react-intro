ec278fb15d53515f6b66b261e894f6d6eb00973e
Lena <fatmeow@gmail.com>
2018-03-10 16:58:34 -0500

    User Router and routes to add component pages

--- dentist-website/src/App.js
+++ dentist-website/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import Contact from './Contact';
 import Procedures from './Procedures';
 
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home></Home>
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/procedures" component={Procedures} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </Router>
     );
   }
 }
