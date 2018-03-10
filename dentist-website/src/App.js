f54731c65abbe72b20a68a06f7ae9551e72e2522
Lena <fatmeow@gmail.com>
2018-03-10 16:34:09 -0500

    Import Home component to app.js

--- dentist-website/src/App.js
+++ dentist-website/src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Home from './Home'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Dentist Website</h1>
-        </header>
-        <p className="App-intro">
-          Welcome to the dentist office
-        </p>
+        <Home></Home>
       </div>
     );
   }
---
+++ dentist-website/src/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Dentist Website</h1>
+        </header>
+        <p>
+          Welcome to the dentist office
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
